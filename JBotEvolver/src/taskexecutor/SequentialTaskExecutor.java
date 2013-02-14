567e4cb3b64c843027aade0631ec9c93344d6586
sancho.oliveira@gmail.com <sancho.oliveira@gmail.com@e8841071-eecf-a525-aefd-766b58e66e4d>
2013-02-14 22:03:06 +0000

    Sequential Task Executor

--- JBotEvolver/src/taskexecutor/SequentialTaskExecutor.java
+++ JBotEvolver/src/taskexecutor/SequentialTaskExecutor.java
@@ -1,7 +1,9 @@
 package taskexecutor;
 
 import java.util.LinkedList;
+
 import evolutionaryrobotics.JBotEvolver;
+import evolutionaryrobotics.MainExecutor;
 import result.Result;
 import simulation.util.Arguments;
 import tasks.Task;
@@ -11,63 +13,55 @@ public class SequentialTaskExecutor extends TaskExecutor {
 	private LinkedList<Task> tasksToDo = new LinkedList<Task>();
 	private LinkedList<Task> tasksDone = new LinkedList<Task>();
 
-	public SequentialTaskExecutor(JBotEvolver jBotEvolver, Arguments args) {
-		super(jBotEvolver, args);
+	public SequentialTaskExecutor(MainExecutor executor,
+			JBotEvolver jBotEvolver, Arguments args) {
+		super(executor, jBotEvolver, args);
 	}
 
 	@Override
 	public void addTask(Task t) {
 		synchronized (tasksToDo) {
 			tasksToDo.add(t);
-		}
-		synchronized (this) {
-			notifyAll();
+			tasksToDo.notifyAll();
 		}
 	}
 
 	@Override
 	public Result getResult() {
-		synchronized (this) {
+		synchronized (tasksDone) {
 			while (tasksDone.isEmpty()) {
 				try {
-					wait();
+					tasksDone.wait();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
+			Task t = tasksDone.pollFirst();
+			return t.getResult();
 		}
-		Task t;
-		synchronized (tasksDone) {
-			t = tasksDone.pollFirst();
-		}
-		return t.getResult();
 	}
 
 	@Override
 	public void run() {
 
 		while (true) {
+			Task t;
 			try {
-				synchronized (this) {
-					while (tasksToDo.isEmpty())
-						wait();
+				synchronized (tasksToDo) {
+					while (tasksToDo.isEmpty()) {
+						tasksToDo.wait();
+					}
+					t = tasksToDo.pollFirst();
+				}
+				t.run();
+				synchronized (tasksDone) {
+					tasksDone.add(t);
+					tasksDone.notifyAll();
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
-			Task t;
-
-			synchronized (tasksToDo) {
-				t = tasksToDo.pollFirst();
-			}
-
-			t.run();
-
-			synchronized (this) {
-				tasksDone.add(t);
-				notifyAll();
-			}
 		}
 	}
 }
