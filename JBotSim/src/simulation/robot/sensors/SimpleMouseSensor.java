4cf1e2cc18040087c7ddbf7c7c5c191bdd1b00b5
tlsrs@iscte-iul.pt <tlsrs@iscte-iul.pt@e8841071-eecf-a525-aefd-766b58e66e4d>
2014-03-06 22:44:05 +0000

    Corrections on the Simple Mouse Sensor

--- JBotSim/src/simulation/robot/sensors/SimpleMouseSensor.java
+++ JBotSim/src/simulation/robot/sensors/SimpleMouseSensor.java
@@ -1,27 +1,16 @@
 package simulation.robot.sensors;
 
-import java.util.ArrayList;
-
 import simulation.Simulator;
-import simulation.physicalobjects.PhysicalObject;
 import simulation.physicalobjects.checkers.AllowMouseChecker;
 import simulation.robot.Robot;
 import simulation.util.Arguments;
 
 public class SimpleMouseSensor extends SimpleLightTypeSensor {
 
-	public SimpleMouseSensor(Simulator simulator, int id, Robot robot,
-			Arguments args) {
+	public SimpleMouseSensor(Simulator simulator, int id, Robot robot, Arguments args) {
 		super(simulator, id, robot, args);
 		setAllowedObjectsChecker(new AllowMouseChecker(id));
-		
-		initAngles();
 	}
 
-	private void initAngles() {
-		angles[1] = -Math.PI/6;
-		angles[0] = Math.PI/6;
-	}
-	
 }
 
