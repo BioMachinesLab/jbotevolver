da402ca27e7d7a4e70d94737a62ce1e50c7601d8
miguelduarte42@gmail.com <miguelduarte42@gmail.com@e8841071-eecf-a525-aefd-766b58e66e4d>
2014-04-17 15:40:06 +0000

    Added SingleSampleGenerationalEvolution

---
+++ JBotEvolver/src/evolutionaryrobotics/evolution/SingleSampleGenerationalEvolution.java
@@ -0,0 +1,82 @@
+package evolutionaryrobotics.evolution;
+
+import java.util.Random;
+
+import simulation.util.Arguments;
+import taskexecutor.TaskExecutor;
+import taskexecutor.results.PostEvaluationResult;
+import taskexecutor.results.SimpleFitnessResult;
+import taskexecutor.tasks.SimpleSampleTask;
+import evolutionaryrobotics.JBotEvolver;
+import evolutionaryrobotics.evolution.GenerationalEvolution;
+import evolutionaryrobotics.neuralnetworks.Chromosome;
+
+public class SingleSampleGenerationalEvolution extends GenerationalEvolution {
+	
+	public SingleSampleGenerationalEvolution(JBotEvolver jBotEvolver, TaskExecutor taskExecutor, Arguments args) {
+		super(jBotEvolver, taskExecutor, args);
+	}
+	
+	@Override
+	public void executeEvolution() {
+		
+		if(population.getNumberOfCurrentGeneration() == 0)
+			population.createRandomPopulation();
+		
+		int samples = population.getNumberOfSamplesPerChromosome();
+		
+		if(!population.evolutionDone()) {
+			taskExecutor.prepareArguments(jBotEvolver.getArguments());
+			taskExecutor.setTotalNumberOfTasks((population.getNumberOfGenerations()-population.getNumberOfCurrentGeneration())*population.getPopulationSize()*samples);
+		}
+		
+		double highestFitness = 0;
+		
+		while(!population.evolutionDone()) {
+			
+			double d = Double.valueOf(df.format(highestFitness));
+			taskExecutor.setDescription(output+" "+population.getNumberOfCurrentGeneration()+"/"+population.getNumberOfGenerations() + " " + d);
+			
+			Chromosome c;
+			
+			int totalSamples = 0;
+			
+			while ((c = population.getNextChromosomeToEvaluate()) != null) {
+				
+				Random r = new Random(population.getGenerationRandomSeed());
+				for(int i = 0 ; i < samples ; i++) {
+					taskExecutor.addTask(new SimpleSampleTask(c.getID(),new JBotEvolver(jBotEvolver.getArgumentsCopy(), jBotEvolver.getRandomSeed()),i,c,r.nextLong()));
+					print(".");
+				}
+				totalSamples+=samples;
+			}
+			
+			print("\n");
+			
+			double values[] = new double[population.getPopulationSize()];
+			
+			while(totalSamples-- > 0) {
+				PostEvaluationResult result = (PostEvaluationResult)taskExecutor.getResult();
+				print("!");
+				values[result.getRun()]+=result.getFitness();
+			}
+			
+			for(int i = 0; i < values.length ; i++) {
+				population.setEvaluationResultForId(i,values[i]/samples);
+			}
+			
+			print("\nGeneration "+population.getNumberOfCurrentGeneration()+
+					"\tHighest: "+population.getHighestFitness()+
+					"\tAverage: "+population.getAverageFitness()+
+					"\tLowest: "+population.getLowestFitness()+"\n");
+			
+			try {
+				diskStorage.savePopulation(population);
+			} catch(Exception e) {e.printStackTrace();}
+			
+			highestFitness = population.getHighestFitness();
+			population.createNextGeneration();
+			
+		}
+	}
+}
