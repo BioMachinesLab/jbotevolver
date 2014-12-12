12f8d402d868d8be4a81128e13d597d5224a1157
miguelduarte42@gmail.com <miguelduarte42@gmail.com@e8841071-eecf-a525-aefd-766b58e66e4d>
2014-12-12 13:12:05 +0000

    Uses the System's look and feel instead of Metal

--- JBotEvolver/src/gui/CombinedGui.java
+++ JBotEvolver/src/gui/CombinedGui.java
@@ -7,12 +7,22 @@ import gui.evolution.EvolutionGui;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class CombinedGui extends JFrame {
 	
 	private JTabbedPane tabbedPane;
 	
 	public CombinedGui(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
+		
 		tabbedPane = new JTabbedPane();
 		
 		EvolutionGui evolution = new EvolutionGui();
