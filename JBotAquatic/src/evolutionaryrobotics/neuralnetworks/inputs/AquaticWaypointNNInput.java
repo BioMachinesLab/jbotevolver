0d7f3d6bfbb17c0d1a9a447c44d82ab9fa1fd2b8
tlsrs@iscte-iul.pt <tlsrs@iscte-iul.pt@e8841071-eecf-a525-aefd-766b58e66e4d>
2015-03-03 18:48:02 +0000

    WaypointSensor and WaypointSensorNNInput commented to remove project errors

--- JBotAquatic/src/evolutionaryrobotics/neuralnetworks/inputs/AquaticWaypointNNInput.java
+++ JBotAquatic/src/evolutionaryrobotics/neuralnetworks/inputs/AquaticWaypointNNInput.java
@@ -1,32 +1,30 @@
 package evolutionaryrobotics.neuralnetworks.inputs;
 
-import simulation.robot.sensor.WaypointSensor;
-import simulation.robot.sensors.Sensor;
 
-public class AquaticWaypointNNInput extends NNInput {
-
-	public AquaticWaypointNNInput(Sensor s) {
-		super(s);
-	}
-
-	@Override
-	public int getNumberOfInputValues() {
-		return 2;
-	}
-
-	@Override
-	public double getValue(int index) {
-		if (index == 0) {
-			return sensor.getSensorReading(index) / 360.0 + 0.5;
-		} else {
-			double range = ((WaypointSensor) sensor).getRange();
-			double distance = sensor.getSensorReading(index);
-
-			if (distance > range)
-				return 0;
-			else
-				return (range - distance) / range;
-
-		}
-	}
-}
+//public class AquaticWaypointNNInput extends NNInput {
+//
+//	public AquaticWaypointNNInput(Sensor s) {
+//		super(s);
+//	}
+//
+//	@Override
+//	public int getNumberOfInputValues() {
+//		return 2;
+//	}
+//
+//	@Override
+//	public double getValue(int index) {
+//		if (index == 0) {
+//			return sensor.getSensorReading(index) / 360.0 + 0.5;
+//		} else {
+//			double range = ((WaypointSensor) sensor).getRange();
+//			double distance = sensor.getSensorReading(index);
+//
+//			if (distance > range)
+//				return 0;
+//			else
+//				return (range - distance) / range;
+//
+//		}
+//	}
+//}
