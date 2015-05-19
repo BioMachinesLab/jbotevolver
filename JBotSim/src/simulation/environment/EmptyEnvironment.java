1897687c8da061fb5cfc1132f0f2cfd6e45424c6
Miguel Duarte <miguelduarte42@gmail.com>
2015-05-19 18:47:40 +0100

    Added a new option to the EmptyEnvironment

--- JBotSim/src/simulation/environment/EmptyEnvironment.java
+++ JBotSim/src/simulation/environment/EmptyEnvironment.java
@@ -1,20 +1,33 @@
 package simulation.environment;
 
 import simulation.Simulator;
+import simulation.robot.Robot;
 import simulation.util.Arguments;
 
 public class EmptyEnvironment extends Environment {
 
 	private double forageLimit;
 	private double forbiddenArea;
+	private double distance;
 	
 	public EmptyEnvironment(Simulator simulator, Arguments arguments) {
 		super(simulator, arguments);
+		distance = arguments.getArgumentAsDoubleOrSetDefault("distance", distance);
 	}
 	
 	@Override
 	public void setup(Simulator simulator) {
 		super.setup(simulator);
+		
+		if(distance > 0) {
+			for(Robot r : robots) {
+				double x = simulator.getRandom().nextDouble()*distance*2-distance;
+				double y = simulator.getRandom().nextDouble()*distance*2-distance;
+				r.setPosition(x, y);
+				r.setOrientation(simulator.getRandom().nextDouble()*Math.PI*2);
+			}
+		}
+		
 	}
 	
 	public double getForageRadius() {
