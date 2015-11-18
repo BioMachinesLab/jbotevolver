d732f518b24ebfcf9caa52b63f7f8680e65ca9e7
Miguel Duarte <miguelduarte42@gmail.com>
2015-11-18 17:06:50 +0000

    Commit after field tests and EvoRobot paper

--- EvolutionAutomator/src/Main.java
+++ EvolutionAutomator/src/Main.java
@@ -210,7 +210,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Main main = new Main(args);
-//		Main main = new Main(new String[]{"rudder.conf"});
+//		Main main = new Main(new String[]{"rudder_final.conf"});
 		main.execute();
 	}
 }
