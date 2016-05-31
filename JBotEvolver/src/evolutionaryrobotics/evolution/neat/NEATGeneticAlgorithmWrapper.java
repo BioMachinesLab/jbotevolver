2fb72ae7eaa36ac1560e48097e14cd1957694c7f
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-05-31 16:30:11 +0100

    Added the possibility to use an alternative task type in NEAT GA wrapper
    The alternative task type can be used through the argument
    task=(classname=xxxx), in the --evolution tag in the config file

--- JBotEvolver/src/evolutionaryrobotics/evolution/neat/NEATGeneticAlgorithmWrapper.java
+++ JBotEvolver/src/evolutionaryrobotics/evolution/neat/NEATGeneticAlgorithmWrapper.java
@@ -1,59 +1,74 @@
 package evolutionaryrobotics.evolution.neat;
 
-import taskexecutor.results.SimpleFitnessResult;
-import taskexecutor.tasks.GenerationalTask;
 import evolutionaryrobotics.JBotEvolver;
 import evolutionaryrobotics.evolution.NEATEvolution;
-import evolutionaryrobotics.evolution.neat.core.InnovationDatabase;
 import evolutionaryrobotics.evolution.neat.core.NEATGADescriptor;
 import evolutionaryrobotics.evolution.neat.core.NEATGeneticAlgorithm;
-import evolutionaryrobotics.evolution.neat.core.NEATPopulation4J;
 import evolutionaryrobotics.evolution.neat.ga.core.Chromosome;
-import evolutionaryrobotics.evolution.neat.ga.core.Species;
 import evolutionaryrobotics.populations.NEATPopulation;
+import simulation.util.Arguments;
+import simulation.util.Factory;
+import taskexecutor.results.SimpleFitnessResult;
+import taskexecutor.tasks.GenerationalTask;
+import tasks.Task;
 
 public class NEATGeneticAlgorithmWrapper extends NEATGeneticAlgorithm {
-	
+
 	protected NEATEvolution evo;
 
 	public NEATGeneticAlgorithmWrapper(NEATGADescriptor descriptor, NEATEvolution evo) {
 		super(descriptor);
 		this.evo = evo;
 	}
-	
+
+	@Override
 	protected void evaluatePopulation(Chromosome[] genotypes) {
-        int i;
-        
-        evolutionaryrobotics.neuralnetworks.Chromosome[] convertedGenotypes =
-        		new evolutionaryrobotics.neuralnetworks.Chromosome[genotypes.length];
-        
-        for (i = 0; i < genotypes.length && evo.continueExecuting(); i++) {
-        	
-        	int samples = evo.getPopulation().getNumberOfSamplesPerChromosome();
-        	
-        	Chromosome neatChromosome = genotypes[i];
-        	
-        	evolutionaryrobotics.neuralnetworks.Chromosome jBotChromosome =
-        			((NEATPopulation)evo.getPopulation()).convertChromosome(neatChromosome, i);
-        	
-        	convertedGenotypes[i] = jBotChromosome;
-        	
-        	GenerationalTask neatTask = new GenerationalTask(
-        			new JBotEvolver(evo.getJBotEvolver().getArgumentsCopy(),
-        			evo.getJBotEvolver().getRandomSeed()),
-        			samples, jBotChromosome, evo.getPopulation().getGenerationRandomSeed());
-        	evo.getTaskExecutor().addTask(neatTask);
-    		System.out.print(".");
-        }
-        
-        System.out.println();
-        
-        for(i = 0 ; i < genotypes.length && evo.continueExecuting() ; i++) {
-        	SimpleFitnessResult r = (SimpleFitnessResult)evo.getTaskExecutor().getResult();
-        	evo.getPopulation().setEvaluationResult(convertedGenotypes[r.getChromosomeId()],r.getFitness());
-        	genotypes[r.getChromosomeId()].updateFitness(r.getFitness());
-        	System.out.print("!");
-        }
-    }
-	
+		int i;
+
+		evolutionaryrobotics.neuralnetworks.Chromosome[] convertedGenotypes = new evolutionaryrobotics.neuralnetworks.Chromosome[genotypes.length];
+
+		for (i = 0; i < genotypes.length && evo.continueExecuting(); i++) {
+
+			int samples = evo.getPopulation().getNumberOfSamplesPerChromosome();
+			Chromosome neatChromosome = genotypes[i];
+
+			evolutionaryrobotics.neuralnetworks.Chromosome jBotChromosome = ((NEATPopulation) evo.getPopulation())
+					.convertChromosome(neatChromosome, i);
+
+			convertedGenotypes[i] = jBotChromosome;
+
+			String taskClass = GenerationalTask.class.getName();
+			if (evo.getJBotEvolver().getArgumentsCopy().get("--evolution").getArgumentIsDefined("task")) {
+				Arguments evolutionArguments = new Arguments(
+						evo.getJBotEvolver().getArgumentsCopy().get("--evolution").getArgumentAsString("task"));
+				if (evolutionArguments.getArgumentIsDefined("classname")) {
+					taskClass = evolutionArguments.getArgumentAsString("classname");
+				}
+			}
+
+			Task task = (Task) Factory.getInstance(taskClass,
+					new JBotEvolver(evo.getJBotEvolver().getArgumentsCopy(), evo.getJBotEvolver().getRandomSeed()),
+					samples, jBotChromosome, evo.getPopulation().getGenerationRandomSeed());
+
+			// GenerationalTask neatTask = new GenerationalTask(
+			// new JBotEvolver(evo.getJBotEvolver().getArgumentsCopy(),
+			// evo.getJBotEvolver().getRandomSeed()),
+			// samples, jBotChromosome,
+			// evo.getPopulation().getGenerationRandomSeed());
+			// System.out.println(evo.getJBotEvolver().getArgumentsCopy());
+
+			evo.getTaskExecutor().addTask(task);
+			System.out.print(".");
+		}
+
+		System.out.println();
+
+		for (i = 0; i < genotypes.length && evo.continueExecuting(); i++) {
+			SimpleFitnessResult r = (SimpleFitnessResult) evo.getTaskExecutor().getResult();
+			evo.getPopulation().setEvaluationResult(convertedGenotypes[r.getChromosomeId()], r.getFitness());
+			genotypes[r.getChromosomeId()].updateFitness(r.getFitness());
+			System.out.print("!");
+		}
+	}
+
 }
