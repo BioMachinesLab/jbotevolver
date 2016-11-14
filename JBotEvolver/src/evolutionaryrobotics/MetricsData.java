e6f9e981a9f6bb0b991cfe704c54476d0dfb4f1b
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-11-14 16:45:10 +0000

    Added documentation and metrics combine method

--- JBotEvolver/src/evolutionaryrobotics/MetricsData.java
+++ JBotEvolver/src/evolutionaryrobotics/MetricsData.java
@@ -1,6 +1,8 @@
 package evolutionaryrobotics;
 
-public class MetricsData {
+import java.util.List;
+
+public abstract class MetricsData {
 	private int generation = -1;
 
 	public MetricsData(int generation) {
@@ -10,4 +12,15 @@ public class MetricsData {
 	public int getGeneration() {
 		return generation;
 	}
+
+	/**
+	 * This method merges the given MetricsData with the current metrics data in
+	 * Case the generation number is equal. The different values are combined
+	 * Using a mean value for each field
+	 * 
+	 * @param metricsData
+	 *            Are the metrics data instances to merge with the current
+	 *            Instance
+	 **/
+	public abstract void combineMetricsData(List<MetricsData> metricsData);
 }
