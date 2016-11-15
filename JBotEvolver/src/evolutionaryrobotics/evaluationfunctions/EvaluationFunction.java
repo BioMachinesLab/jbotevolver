d5dc8297f8c66b1f90cec7697892ba07e9e6ac3d
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-11-15 18:09:01 +0000

    Added collect metrics flag and metricsData variable

--- JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/EvaluationFunction.java
+++ JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/EvaluationFunction.java
@@ -8,33 +8,35 @@ import simulation.Updatable;
 import simulation.util.Arguments;
 
 public abstract class EvaluationFunction implements Serializable, Updatable {
+	private static final long serialVersionUID = -3895531956415747801L;
 	protected double fitness;
 	protected MetricsData metricsData;
-	protected Arguments args;
+	protected Arguments arguments;
+	protected boolean collectMetrics = false;
 
-	public EvaluationFunction(Arguments args) {
-		this.args = args;
+	public EvaluationFunction(Arguments arguments) {
+		this.arguments = arguments;
+		collectMetrics = arguments.getArgumentAsIntOrSetDefault("collectMetrics", 0) == 1;
 	}
 
 	public double getFitness() {
 		return fitness;
 	}
-	
+
 	public Arguments getArgs() {
-		return args;
+		return arguments;
 	}
-	
-	public MetricsData getMetricsData(){
+
+	public MetricsData getMetricsData() {
 		return metricsData;
 	}
-	
-	
+
 	public static EvaluationFunction getEvaluationFunction(Arguments arguments) {
 		if (!arguments.getArgumentIsDefined("classname"))
-			throw new RuntimeException("Evaluation 'classname' not defined: "+arguments.toString());
+			throw new RuntimeException("Evaluation 'classname' not defined: " + arguments.toString());
 
 		String evaluationName = arguments.getArgumentAsString("classname");
-		
+
 		try {
 			Constructor<?>[] constructors = Class.forName(evaluationName).getDeclaredConstructors();
 			for (Constructor<?> constructor : constructors) {
@@ -44,16 +46,16 @@ public abstract class EvaluationFunction implements Serializable, Updatable {
 				}
 			}
 		} catch (Exception e) {
-			
-			// NEAT needs to instantiate EvalFunctions locally, so it is necessary to check this
-			// if we are using Conillon...
-			if(evaluationName.startsWith("__")) {
+
+			// NEAT needs to instantiate EvalFunctions locally, so it is
+			// Necessary to check this if we are using Conillon...
+			if (evaluationName.startsWith("__")) {
 				System.out.println(evaluationName);
-				evaluationName = evaluationName.substring(evaluationName.indexOf('.')+1,evaluationName.length());
+				evaluationName = evaluationName.substring(evaluationName.indexOf('.') + 1, evaluationName.length());
 				arguments.setArgument("classname", evaluationName);
 				return getEvaluationFunction(arguments);
 			}
-						
+
 			e.printStackTrace();
 			System.exit(-1);
 		}
