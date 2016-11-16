d60acd6990ddf0a9295e8653862c3eec8a6d4f54
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-11-16 18:34:34 +0000

    Moved metrics plot to left wing and created a metrics display on right

--- JBotVasco/src/gui/FormationCIResultViewerGui.java
+++ JBotVasco/src/gui/FormationCIResultViewerGui.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -7,18 +9,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.JViewport;
+import javax.swing.KeyStroke;
 import javax.swing.tree.TreePath;
 
 import evolutionaryrobotics.JBotEvolver;
-import gui.util.SpringUtilities;
+import evolutionaryrobotics.MetricsData;
 import gui.utils.FormationsMetricsGraphPlotter;
 import gui.utils.FormationsMetricsGraphPlotter.MetricsType;
 import simulation.JBotSim;
@@ -31,6 +43,12 @@ public class FormationCIResultViewerGui extends CIResultViewerGui {
 	private JButton timeUntilFirstOccupationButton;
 	private JButton permutationMetricButton;
 	private JButton reocupationTimeButton;
+
+	private boolean createdMetricsPanel = false;
+	private JPanel mainMetricsPanel;
+	private JPanel metricsPanel;
+	private JCheckBox collectMetricsCheckBox;
+	private HashMap<Method, JTextField> methodsTextFields;
 
 	public FormationCIResultViewerGui(JBotSim jBotEvolver, Arguments args) {
 		super(jBotEvolver, args);
@@ -47,41 +65,213 @@ public class FormationCIResultViewerGui extends CIResultViewerGui {
 		Container panel = super.initRightWrapperPanel();
 
 		if (enableDebugOptions) {
-			// Metrics panel
-			JPanel metricsPanel = new JPanel(new GridLayout(4, 1));
-			metricsPanel.setBorder(BorderFactory.createTitledBorder("Metrics"));
+			// Metrics info
+			mainMetricsPanel = new JPanel(new BorderLayout());
+			mainMetricsPanel.setBorder(BorderFactory.createTitledBorder("Metrics"));
+			collectMetricsCheckBox = new JCheckBox("Collect metrics");
+			collectMetricsCheckBox.setSelected(true);
+			collectMetricsCheckBox.setHorizontalAlignment(JCheckBox.CENTER);
 
-			timeInsideRobotMetricsButton = new JButton("Time inside spots");
-			timeUntilFirstOccupationButton = new JButton("Time until first occupation");
-			permutationMetricButton = new JButton("Occupied spots count");
-			reocupationTimeButton = new JButton("Reocupation time");
+			mainMetricsPanel.add(collectMetricsCheckBox, BorderLayout.NORTH);
 
-			metricsPanel.add(timeInsideRobotMetricsButton);
-			metricsPanel.add(timeUntilFirstOccupationButton);
-			metricsPanel.add(permutationMetricButton);
-			metricsPanel.add(reocupationTimeButton);
-
-			debugOptions.add(metricsPanel);
-
-			SpringUtilities.makeGrid(debugOptions, 4, 1, // rows, cols
-					0, 0, // initialX, initialY
-					10, 10);
+			debugOptions.add(mainMetricsPanel);
 		}
 
-		// @vasco: 1200 Pixels is hardcoded value and 300 came from super
-		// Class
-		final int HEIGHT = 1200;
-		final int WIDTH = 300;
 		if (panel instanceof JScrollPane) {
 			JViewport viewport = ((JScrollPane) panel).getViewport();
-			((JPanel) viewport.getView()).setPreferredSize(new Dimension(WIDTH, HEIGHT));
-
+			viewport.setLayout(new ConstrainedViewPortLayout());
 			return panel;
 		} else {
-			panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-			return new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+			JScrollPane jScroll = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 					JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+			JViewport viewport = jScroll.getViewport();
+			viewport.setLayout(new ConstrainedViewPortLayout());
+			return jScroll;
 		}
+	}
+
+	@Override
+	protected JPanel initLeftWrapperPanel() {
+		/*
+		 * Top section
+		 */
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+		topPanel.add(currentFileTextField);
+		currentFileTextField.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		// File tree
+		treeWrapper = new JPanel(new BorderLayout());
+		fileTree = new FileTree(new File("."));
+		topPanel.add(fileTree);
+		fileTree.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		topPanel.add(fitnessSummary);
+		fitnessSummary.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		// Buttons
+		JPanel editLoadPanel = new JPanel(new GridLayout(1, 3));
+		editLoadPanel.add(openButton);
+		editLoadPanel.add(editButton);
+		editLoadPanel.add(loadButton);
+		editLoadPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		JPanel actionsButtonsPanel = new JPanel(new BorderLayout());
+		actionsButtonsPanel.setBorder(BorderFactory.createTitledBorder("Actions"));
+		actionsButtonsPanel.add(editLoadPanel, BorderLayout.NORTH);
+		actionsButtonsPanel.add(compareFitnessButton, BorderLayout.CENTER);
+		actionsButtonsPanel.add(plotFitnessButton, BorderLayout.SOUTH);
+
+		actionsButtonsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		topPanel.add(actionsButtonsPanel);
+
+		// Metrics panel
+		JPanel metricsPanel = new JPanel(new GridLayout(4, 1));
+		metricsPanel.setBorder(BorderFactory.createTitledBorder("Metrics plot"));
+
+		timeInsideRobotMetricsButton = new JButton("Time inside spots");
+		timeUntilFirstOccupationButton = new JButton("Time until first occupation");
+		permutationMetricButton = new JButton("Occupied spots count");
+		reocupationTimeButton = new JButton("Reocupation time");
+
+		metricsPanel.add(timeInsideRobotMetricsButton);
+		metricsPanel.add(timeUntilFirstOccupationButton);
+		metricsPanel.add(permutationMetricButton);
+		metricsPanel.add(reocupationTimeButton);
+		topPanel.add(metricsPanel);
+
+		/*
+		 * Arguments panel
+		 */
+		JPanel argumentsPanel = new JPanel(new BorderLayout());
+		extraArguments = new JEditorPane();
+		argumentsPanel.setBorder(BorderFactory.createTitledBorder("Extra arguments"));
+		argumentsPanel.add(new JScrollPane(extraArguments), BorderLayout.CENTER);
+		argumentsPanel.add(newRandomSeedButton, BorderLayout.SOUTH);
+
+		extraArguments.getInputMap().put(KeyStroke.getKeyStroke("control LEFT"), "none");
+		extraArguments.getInputMap().put(KeyStroke.getKeyStroke("control RIGHT"), "none");
+
+		/*
+		 * Join everything
+		 */
+		treeWrapper.add(topPanel, BorderLayout.NORTH);
+		treeWrapper.add(argumentsPanel, BorderLayout.CENTER);
+		treeWrapper.setBorder(BorderFactory.createTitledBorder("Experiments"));
+
+		return treeWrapper;
+	}
+
+	@Override
+	protected void updateStatus() {
+		if (!createdMetricsPanel && collectMetricsCheckBox.isSelected()) {
+			HashMap<String, Arguments> args = jBotEvolver.getArguments();
+			if (simulator != null && args != null && evaluationFunction != null) {
+				MetricsData metricsData = evaluationFunction.getMetricsData();
+
+				if (metricsData != null && MetricsData.class.isInstance(metricsData)) {
+					Class<? extends MetricsData> metricsDataClass = metricsData.getClass();
+
+					methodsTextFields = new HashMap<Method, JTextField>();
+					metricsPanel = new JPanel();
+
+					JPanel labelsPanel = new JPanel();
+					JPanel textFieldsPanel = new JPanel();
+					ArrayList<Method> methods = new ArrayList<Method>();
+					for (Method method : metricsDataClass.getDeclaredMethods()) {
+						if (method.getName().contains("get")) {
+							methods.add(method);
+						}
+					}
+
+					methods.sort(new Comparator<Method>() {
+
+						@Override
+						public int compare(Method method1, Method method2) {
+							String method1Name = method1.getName();
+							String method2Name = method2.getName();
+
+							return method1Name.compareTo(method2Name);
+						}
+					});
+
+					for (Method method : methods) {
+						String[] split = method.getName().split("\\.");
+						String name = split[split.length - 1].replace("get", "");
+
+						labelsPanel.add(new JLabel(name + ":"));
+
+						JTextField textField = new JTextField("N/A");
+						textField.setHorizontalAlignment(JTextField.CENTER);
+						textField.setEditable(false);
+
+						Dimension preferredSize = textField.getPreferredSize();
+						preferredSize.width += 10;
+						textField.setPreferredSize(preferredSize);
+						textFieldsPanel.add(textField);
+
+						methodsTextFields.put(method, textField);
+					}
+
+					labelsPanel.setLayout(new GridLayout(methods.size(), 1));
+					textFieldsPanel.setLayout(new GridLayout(methods.size(), 1));
+
+					metricsPanel.setLayout(new BorderLayout());
+					metricsPanel.add(labelsPanel, BorderLayout.WEST);
+					metricsPanel.add(textFieldsPanel, BorderLayout.CENTER);
+
+					mainMetricsPanel.add(metricsPanel, BorderLayout.CENTER);
+
+					mainMetricsPanel.validate();
+					mainMetricsPanel.repaint();
+
+					debugOptions.repaint();
+
+					if (rightWrapperPanel instanceof JScrollPane) {
+						JViewport viewport = ((JScrollPane) rightWrapperPanel).getViewport();
+						Component[] components = viewport.getComponents();
+
+						for (Component component : components) {
+							component.validate();
+							component.repaint();
+						}
+					}
+
+					rightWrapperPanel.repaint();
+					validate();
+
+					createdMetricsPanel = true;
+				}
+			}
+		} else if (collectMetricsCheckBox.isSelected()) {
+			MetricsData metricsData = evaluationFunction.getMetricsData();
+			for (Method method : methodsTextFields.keySet()) {
+				try {
+					Object o = method.invoke(metricsData);
+					methodsTextFields.get(method).setText(o.toString());
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		super.updateStatus();
+	}
+
+	@Override
+	public Simulator loadSimulator() {
+		Simulator simulator = super.loadSimulator();
+		if (simulator != null) {
+			if (evaluationFunction.getMetricsData() == null) {
+				evaluationFunction.setCollectMetrics(collectMetricsCheckBox.isSelected());
+
+				if (collectMetricsCheckBox.isSelected()) {
+					createdMetricsPanel = false;
+				}
+			}
+		}
+
+		return simulator;
 	}
 
 	@Override
