414cdfe1313c3ec7cbf24c77da67af2ff1dd3da5
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-12-09 18:47:23 +0000

    Added a MapViewer implementation and the needed libraries

---
+++ LogProcessor/src/gui/map/UpdatePanel.java
@@ -0,0 +1,13 @@
+package gui.map;
+
+import javax.swing.JPanel;
+
+import threads.UpdateThread;
+
+public abstract class UpdatePanel extends JPanel{
+	private static final long serialVersionUID = -6024147826485066415L;
+	public abstract void registerThread(UpdateThread t);
+	public abstract void threadWait();
+	public abstract long getSleepTime();
+
+}
