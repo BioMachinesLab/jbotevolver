1c86d1a99872f0b796238799be5f41ba55d9b2fd
RitaRamos <rita.mparada.ramos@gmail.com>
2018-02-02 18:10:11 +0000

    Remove unnecessary files and update EmptyEnvironment to have option of putting Walls and refactor IntensityForaging

--- JBotRita/src/enviromentsSwarms/EmptyEnviromentsWithFixPositions.java
+++ JBotRita/src/enviromentsSwarms/EmptyEnviromentsWithFixPositions.java
@@ -4,99 +4,61 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import physicalobjects.WallWithZ;
 import simulation.Simulator;
 import simulation.environment.Environment;
 import simulation.robot.Robot;
 import simulation.util.Arguments;
+import simulation.util.ArgumentsAnnotation;
 
 public class EmptyEnviromentsWithFixPositions extends Environment{
 
-	private double forageLimit;
-	private double forbiddenArea;
-	//private double distance;
+	@ArgumentsAnnotation(name="distance", defaultValue="0.20")	
+	private double distance;
 	
+	@ArgumentsAnnotation(name = "putWalls", values={"0","1"})
+	protected boolean putWalls;
+	
+	protected double wallsSafeMargin = 0.2;
+	protected double wallsDensity = 0.08;
+	
+	protected Simulator simulator;
+
+
 	public EmptyEnviromentsWithFixPositions(Simulator simulator, Arguments arguments) {
 		super(simulator, arguments);
-		//distance =2;
+		distance = arguments.getArgumentIsDefined("distance") ? arguments
+				.getArgumentAsInt("distance") : 0.20;
+		putWalls   = arguments.getArgumentIsDefined("putWalls") ? (arguments.getArgumentAsInt("putWalls")==1)	: false;
+		this.simulator = simulator;
 	}
 	
 	@Override
 	public void setup(Simulator simulator) {
 		super.setup(simulator);
-		
-		
 			for(Robot r : robots) {
-//				double x = simulator.getRandom().nextDouble()*distance*2-distance;
-//				double y = simulator.getRandom().nextDouble()*distance*2-distance;
-//				r.setPosition(x, y);
+				double x = simulator.getRandom().nextDouble()*distance*2-distance;
+				double y = simulator.getRandom().nextDouble()*distance*2-distance;
+				r.setPosition(x, y);
 				r.setOrientation(simulator.getRandom().nextDouble()*Math.PI*2);
-				//System.out.println("radius"+r.getOrientation());
 			}
-		
-//			robots.get(0).setPosition(0,0);
-//			robots.get(1).setPosition(-2,0);
-//			robots.get(2).setPosition(2,0);
-//			robots.get(3).setPosition(4,5);
-//			robots.get(4).setPosition(-5,5);
-//			robots.get(1).setOrientation(simulator.getRandom().nextDouble()*Math.PI*2);
-//			robots.get(2).setOrientation(simulator.getRandom().nextDouble()*Math.PI*2);
-
-			//robots.get(4).setPosition(0,1);
-
-
+		if(putWalls)
+			addWalls();
 	}
 	
-	public double getForageRadius() {
-		return forageLimit;
+	protected void addWalls() {
+		addStaticObject(new WallWithZ(simulator, 0, width / 2,
+				width, wallsDensity, 10)); // HorizontalWallNorth
+		addStaticObject(new WallWithZ(simulator, height / 2, 0,
+				wallsDensity, height, 10)); // VerticalEast
+		addStaticObject(new WallWithZ(simulator, 0, -width / 2,
+				width, wallsDensity, 10)); // HorizontalSouth
+		addStaticObject(new WallWithZ(simulator, -height / 2, 0,
+				wallsDensity, height, 10)); // VerticalWest
 	}
 
-	public double getForbiddenArea() {
-		return forbiddenArea;
-	}
-	
 	@Override
-	public void update(double time) {
-		
-	}
+	public void update(double time) {}
 	
-	public List<List<Robot>> getClusters(Collection<Robot> robots) {
-        List<List<Robot>> clusters = new ArrayList<>();
-        // One cluster for each robot
-        for (Robot r : robots) {
-            List<Robot> cluster = new ArrayList<>();
-            cluster.add(r);
-            clusters.add(cluster);
-        }
-
-        boolean merged = true;
-        // stop when the clusters cannot be merged anymore
-        
-        while(merged) {
-            merged = false;
-            // find two existing clusters to merge
-            for (int i = 0; i < clusters.size() && !merged; i++) {
-                for (int j = i + 1; j < clusters.size() && !merged; j++) {
-                    List<Robot> c1 = clusters.get(i);
-                    List<Robot> c2 = clusters.get(j);
-                    // check if the two clusters have (at least) one individual close to the other
-                    for (int ri = 0; ri < c1.size() && !merged; ri++) {
-                        for (int rj = 0; rj < c2.size() && !merged; rj++) {
-                            Robot r1 = c1.get(ri);
-                            Robot r2 = c2.get(rj);
-                            
-                            if (r1.getPosition().distanceTo(r2.getPosition()) - r1.getRadius() - r2.getRadius() <= 0.5) {
-	                            // do the merge
-	                            merged = true;
-	                            clusters.get(i).addAll(clusters.get(j));
-	                            clusters.remove(j);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-       //System.out.println(clusters.size()+"migue");
-        return clusters;
-    }
 }
 	
--- JBotRita/src/enviromentsSwarms/IntensityForagingRobotsEnviroments.java
+++ JBotRita/src/enviromentsSwarms/IntensityForagingRobotsEnviroments.java
@@ -2,19 +2,12 @@ package enviromentsSwarms;
 
 import java.awt.Color;
 
-import collisionHandling.JumpingZCollisionManager;
-import mathutils.Vector2d;
-import net.jafama.FastMath;
 import physicalobjects.IntensityPrey;
-import physicalobjects.WallWithZ;
 import simulation.Simulator;
-import simulation.environment.Environment;
 import simulation.physicalobjects.Prey;
-import simulation.physicalobjects.Wall;
-import simulation.robot.Robot;
 import simulation.util.Arguments;
 import simulation.util.ArgumentsAnnotation;
-import environments_ForagingIntensityPreys.ForagingIntensityPreysEnvironment;
+import environment.ForagingIntensityPreysEnvironment;
 
 public class IntensityForagingRobotsEnviroments extends
 		ForagingIntensityPreysEnvironment {
@@ -22,59 +15,13 @@ public class IntensityForagingRobotsEnviroments extends
 	@ArgumentsAnnotation(name = "preyIntensity", defaultValue = "30.0")
 	protected double intensity;
 	
-	@ArgumentsAnnotation(name = "putWalls", values={"0","1"})
-	protected boolean putWalls;
-	
-
-	protected double limitWidhtWalls = width;
-	protected double limitHeightWalls = height;
-	protected int generation = 0;
-	protected double wallsSafeMargin = 0.2;
-	protected double wallsDensity = 0.08;
-
 	public IntensityForagingRobotsEnviroments(Simulator simulator,
 			Arguments arguments) {
 		super(simulator, arguments);
-		generation = arguments.getArgumentAsInt("generations");
 		intensity = arguments.getArgumentIsDefined("preyIntensity") ? arguments
-				.getArgumentAsInt("preyIntensity") : 30.0;
-				
-		putWalls   = arguments.getArgumentIsDefined("putWalls") ? (arguments.getArgumentAsInt("putWalls")==1)	: false;
-
+				.getArgumentAsDouble("preyIntensity") : 30.0;
 	}
 
-	@Override
-	public void setup(Simulator simulator) {
-		super.setup(simulator);
-		for (int i = 0; i < numberOfPreys; i++) {
-			this.addPrey(new IntensityPrey(simulator, "Prey " + i,
-					randomPosition(), 0, PREY_MASS, 0.1, intensity));
-			// this.addPrey(new IntensityPrey(simulator, "Prey " + i,new
-			// Vector2d(4,4), 0, PREY_MASS, 0.1, intensity));
-			// if(generation > 70){
-			// this.addPrey(new IntensityPrey(simulator, "Prey " + i,new
-			// Vector2d(4,4), 0, PREY_MASS, 0.1, intensity));
-			// }
-			// System.out.println(simulator.getEnvironment().getPrey());
-		}
-		for (Robot r : robots) {
-			r.setOrientation(simulator.getRandom().nextDouble() * Math.PI * 2);
-		}
-		if(putWalls)
-			addWalls();
-	}
-
-	protected void addWalls() {
-		addStaticObject(new WallWithZ(simulator, 0, limitWidhtWalls / 2,
-				limitWidhtWalls, wallsDensity, 10)); // HorizontalWallNorth
-		addStaticObject(new WallWithZ(simulator, limitHeightWalls / 2, 0,
-				wallsDensity, limitHeightWalls, 10)); // VerticalEast
-		addStaticObject(new WallWithZ(simulator, 0, -limitWidhtWalls / 2,
-				limitWidhtWalls, wallsDensity, 10)); // HorizontalSouth
-		addStaticObject(new WallWithZ(simulator, -limitHeightWalls / 2, 0,
-				wallsDensity, limitHeightWalls, 10)); // VerticalWest
-
-	}
 
 	@Override
 	public void update(double time) {
@@ -93,17 +40,6 @@ public class IntensityForagingRobotsEnviroments extends
 				prey.setColor(Color.BLACK);
 		}
 
-	}
-
-	protected Vector2d randomPosition() {
-
-		return new Vector2d(random.nextDouble() * (width - wallsSafeMargin * 2)
-				+ (-(width / 2 - wallsSafeMargin)), random.nextDouble()
-				* (width - wallsSafeMargin * 2)
-				+ (-(width / 2 - wallsSafeMargin)));
-	}
-	
-	
-	
+	}	
 	
 }
