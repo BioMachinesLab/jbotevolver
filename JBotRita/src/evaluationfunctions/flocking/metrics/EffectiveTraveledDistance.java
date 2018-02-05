57b97acaa45a1dc2ed612952736e83c2169eca4e
RitaRamos <rita.mparada.ramos@gmail.com>
2018-02-05 21:07:31 +0000

    Refactor Effective traveled distance metric, and providing doccumentation

--- JBotRita/src/evaluationfunctions/flocking/metrics/EffectiveTraveledDistance.java
+++ JBotRita/src/evaluationfunctions/flocking/metrics/EffectiveTraveledDistance.java
@@ -11,9 +11,11 @@ import evolutionaryrobotics.evaluationfunctions.EvaluationFunction;
 
 
 /**
- * Reward for robots being aligned, ie. matching the orientation
- * (this fitness function is the order metric commonly used in flocking studies,
- * having a value of 1 is the swarm is aligned and a value of 0 when there is no alignment)
+ * Evaluate the distance the swarm traveled into the goal direction
+ * D=(xT −x0)·f
+ * where f is the position vector of the food patch (prey), 
+ * and xT and x0 are the robots’ position vectors at time T and 0, respectively,
+ * being then computed the dot product between the two vectors.
  * @author Rita Ramos
  */
 
@@ -27,25 +29,28 @@ public class EffectiveTraveledDistance extends EvaluationFunction {
 		super(args);	
 	}
 	
+	
 	@Override
 	public double getFitness() {
+		fitness=0;
 		Vector2d preyPosition = simulator.getEnvironment().getPrey().get(0).getPosition();
 		for(Robot r : robots){
+
 			Vector2d xT_x0= new Vector2d(r.getPosition().x- inicialDistanceToPrey_perRobot.get(r).x,r.getPosition().y- inicialDistanceToPrey_perRobot.get(r).y) ;
 			fitness+=xT_x0.x*preyPosition.x + xT_x0.y*preyPosition.y;
 		}
 		return fitness/(double)robots.size();
 	}
 	
+	
 
 	@Override
 	public void update(Simulator simulator) {
-		
 		if(simulator.getTime()==0){  //time == 0
 			this.simulator=simulator;
 			robots=simulator.getEnvironment().getRobots();
 			for(Robot r : robots){
-				inicialDistanceToPrey_perRobot.put(r, r.getPosition());
+				inicialDistanceToPrey_perRobot.put(r, new Vector2d(r.getPosition()));
 			}
 		}
 	}
